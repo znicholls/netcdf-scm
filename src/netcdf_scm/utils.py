c2ed265f94ab7ef254662b9602485e52ba7936ab
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2018-10-12 02:12:20 +0200

    Add utils

---
+++ src/netcdf_scm/utils.py
@@ -0,0 +1,37 @@
+import numpy as np
+import cf_units
+
+
+def get_cube_timeseries_data(scm_cube):
+    """
+
+    """
+    assert_only_cube_dim_coord_is_time(scm_cube)
+    return scm_cube.cube.data
+
+
+def get_scm_cube_time_axis_in_calendar(scm_cube, calendar):
+    """
+
+    """
+    assert_only_cube_dim_coord_is_time(scm_cube)
+    time = scm_cube.cube.dim_coords[0]
+    return cf_units.num2date(time.points, time.units.name, calendar)
+
+
+def assert_only_cube_dim_coord_is_time(scm_cube):
+    """
+
+    """
+    assert_msg = "Should only have time coordinate here"
+    assert len(scm_cube.cube.dim_coords) == 1, assert_msg
+    assert scm_cube.cube.dim_coords[0].standard_name == "time"
+
+
+def assert_all_time_axes_same(time_axes):
+    """
+
+    """
+    for time_axis_to_check in time_axes:
+        assert_msg = "all the time axes should be the same"
+        np.testing.assert_array_equal(time_axis_to_check, time_axes[0]), assert_msg
---
+++ tests/unit/test_utils.py
@@ -0,0 +1,14 @@
+def test_assert_only_cube_dim_coord_is_time():
+    assert False
+
+
+def test_get_cube_timeseries_data():
+    assert False
+
+
+def test_get_cube_time_axis_in_calendar():
+    assert False
+
+
+def test_assert_all_time_axes_same():
+    assert False
