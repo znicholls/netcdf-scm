ab9380c2de96cb3ee7199a66bdffaddf5893bb71
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2018-10-29 14:55:07 +0100

    Add minimal working example of crunching

--- scripts/crunch_to_scm.py
+++ scripts/crunch_to_scm.py
@@ -7,25 +7,35 @@ Improvements in future:
 - output datapackages (speak to Robert)
 """
 
-from os import walk
+from os import walk, makedirs
+from os import path
 from os.path import join
 
 
 from netcdf_scm.iris_cube_wrappers import MarbleCMIP5Cube
+from progressbar import progressbar
 
 
 INPUT_DIR = "tests/test_data/marble_cmip5"
-OUTPUT_DIR = "."
+OUTPUT_DIR = "crunched_files"
+LAND_MASK_THRESHOLD = 50
 
-for (dirpath, dirnames, filenames) in walk(INPUT_DIR):
-    print(dirpath)
-    print(dirnames)
-    print(filenames)
-    print(" ")
+
+if not path.exists(OUTPUT_DIR):
+    makedirs(OUTPUT_DIR)
+
+
+for (dirpath, dirnames, filenames) in progressbar(walk(INPUT_DIR)):
     if 'fx' in dirnames:
         dirnames.remove('fx')  # don't visit fx directories
     elif not dirnames:
         assert len(filenames) == 1
-        import pdb
-        pdb.set_trace()
+        if not filenames[0].endswith(".nc"):
+            continue
+
+        scmcube = MarbleCMIP5Cube()
+        scmcube.load_data_from_path(join(dirpath, filenames[0]))
+        magicc_df = scmcube.get_scm_timeseries(land_mask_threshold=LAND_MASK_THRESHOLD)
+        out_filename = "scm_crunched_{}".format(filenames[0].replace(".nc", ".csv"))
+        magicc_df.df.to_csv(join(OUTPUT_DIR, out_filename), index=False)
 
