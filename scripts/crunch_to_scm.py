9c2caf1db1b0c16e2feb1b9a9735e8fa7e20aade
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2018-10-30 03:18:20 +1100

    Crunch all marble files, note but do not fix errors

--- scripts/crunch_to_scm.py
+++ scripts/crunch_to_scm.py
@@ -16,8 +16,8 @@ from netcdf_scm.iris_cube_wrappers import MarbleCMIP5Cube
 from progressbar import progressbar
 
 
-INPUT_DIR = "tests/test_data/marble_cmip5"
-OUTPUT_DIR = "crunched_files"
+INPUT_DIR = "/data/marble"
+OUTPUT_DIR = "/data/marble/sandbox/znicholls/cmip5_crunched_files"
 LAND_MASK_THRESHOLD = 50
 
 
@@ -28,14 +28,18 @@ if not path.exists(OUTPUT_DIR):
 for (dirpath, dirnames, filenames) in progressbar(walk(INPUT_DIR)):
     if 'fx' in dirnames:
         dirnames.remove('fx')  # don't visit fx directories
+    elif 'cmip5/' not in dirpath:
+        continue 
     elif not dirnames:
-        assert len(filenames) == 1
         if not filenames[0].endswith(".nc"):
             continue
-
-        scmcube = MarbleCMIP5Cube()
-        scmcube.load_data_from_path(join(dirpath, filenames[0]))
-        magicc_df = scmcube.get_scm_timeseries(land_mask_threshold=LAND_MASK_THRESHOLD)
-        out_filename = "scm_crunched_{}".format(filenames[0].replace(".nc", ".csv"))
-        magicc_df.df.to_csv(join(OUTPUT_DIR, out_filename), index=False)
-
+        try:
+            assert len(filenames) == 1
+            scmcube = MarbleCMIP5Cube()
+            scmcube.load_data_from_path(join(dirpath, filenames[0]))
+            magicc_df = scmcube.get_scm_timeseries(land_mask_threshold=LAND_MASK_THRESHOLD)
+            out_filename = "scm_crunched_{}".format(filenames[0].replace(".nc", ".csv"))
+            magicc_df.df.to_csv(join(OUTPUT_DIR, out_filename), index=False)
+        except:
+            print("Failed: {}".format(filenames))
+            continue
