f65b99441ede9f56a7e474e39e7311f4e6e0bb4d
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2018-11-14 16:37:42 +0000

    Tweak crunch script again
    Former-commit-id: 36acdcbe6ff720ee259a11f00eca567644fa97ad

--- scripts/crunch_to_scm.py
+++ scripts/crunch_to_scm.py
@@ -1,21 +1,16 @@
+import sys
+import argparse
 import os
 from os import walk, makedirs, path
 from os.path import join, isfile
 import warnings
 import time
+from time import gmtime, strftime
 
 
+import netcdf_scm
 from netcdf_scm.iris_cube_wrappers import MarbleCMIP5Cube
 import progressbar
-
-
-INPUT_DIR = "./tests/test-data/marble-cmip5"
-OUTPUT_DIR = "./output-examples/crunched-files"
-LAND_MASK_THRESHOLD = 50
-VAR_TO_CRUNCH = "tas"
-
-if not path.exists(OUTPUT_DIR):
-    makedirs(OUTPUT_DIR)
 
 
 def crunch_data(
@@ -24,7 +19,6 @@ def crunch_data(
     var_to_crunch=None,
     land_mask_threshold=50,
     force_regeneration=False,
-    output_prefix="scm_crunched",
 ):
     """Crunch data in a directory structure to OpenSCM csvs
 
@@ -53,7 +47,19 @@ def crunch_data(
         Prefix to attach to the input filenames when saving the
         crunched csvs.
     """
-    print("Crunching:\n{}\n\nto\n{}".format(in_dir, out_dir))
+    output_prefix = "netcdf-scm"
+    separator = "_"
+    timestamp = strftime("%Y%m%d %H%M%S", gmtime())
+    out_sub_dir = "netcdf-scm-crunched"
+    out_dir = join(out_dir, out_sub_dir)
+    print("\nCrunching:\n{}\n\nto\n{}\n".format(in_dir, out_dir))
+
+    if not path.exists(out_dir):
+        print("Making output directory: {}\n".format(out_dir))
+        makedirs(out_dir)
+
+    already_exist_files = []
+
     time.sleep(0.5)  # needed to get logging bar in right place...
     # really should use a logger here
     with warnings.catch_warnings(record=True) as recorded_warns:
@@ -75,23 +81,40 @@ def crunch_data(
                 format_custom_text.update_mapping(curr_dir=dirpath)
                 bar.update(i)
                 try:
+                    # todo: should be an arg/autodiscovered
                     scmcube = MarbleCMIP5Cube()
                     if len(filenames) == 1:
-                        out_filename = "{}_{}".format(
-                            output_prefix, filenames[0].replace(".nc", ".csv")
+                        out_filename = separator.join(
+                            [output_prefix, filenames[0].replace(".nc", ".csv")]
                         )
-                        outfile = join(out_dir, out_filename)
-                        if not force_regeneration and isfile(outfile):
-                            continue
                         scmcube.load_data_from_path(join(dirpath, filenames[0]))
+                        out_filedir = scmcube._get_data_directory().replace(
+                            scmcube.root_dir, out_dir
+                        )
+                        out_filepath = join(out_filedir, out_filename)
+                        if not path.exists(out_filedir):
+                            makedirs(out_filedir)
+                        if not force_regeneration and isfile(out_filepath):
+                            already_exist_files.append(out_filepath)
+                            continue
                     else:
                         scmcube.load_data_in_directory(dirpath)
-                        out_filename = "{}_{}".format(
-                            output_prefix,
-                            scmcube._get_data_filename().replace(".nc", ".csv"),
+                        out_filename = separator.join(
+                            [
+                                output_prefix,
+                                scmcube._get_data_filename().replace(".nc", ".csv"),
+                            ]
                         )
-                        outfile = join(out_dir, out_filename)
-                        if not force_regeneration and isfile(outfile):
+
+                        out_filedir = scmcube._get_data_directory().replace(
+                            scmcube.root_dir, out_dir
+                        )
+                        out_filepath = join(out_filedir, out_filename)
+                        if not path.exists(out_filedir):
+                            makedirs(out_filedir)
+
+                        if not force_regeneration and isfile(out_filepath):
+                            already_exist_files.append(out_filepath)
                             continue
 
                     magicc_df = scmcube.get_scm_timeseries(
@@ -102,8 +125,8 @@ def crunch_data(
                         index=["time"],
                         columns=["variable", "unit", "region", "model", "scenario"],
                     )
+                    magicc_df.df.to_csv(out_filepath)
 
-                    magicc_df.df.to_csv(outfile)
                 except Exception as exc:
                     header = "Exception"
                     exc_string = header + "\n" + "-" * len(header) + "\n" + str(exc)
@@ -113,27 +136,94 @@ def crunch_data(
                     continue
         bar.finish()
 
-    header_underline = "========"
-    msg_underline = "--------"
-    warnings_together = "\n\n{}\n\n".format(msg_underline).join(
+    header_underline = "="
+    msg_underline = "-"
+
+    warnings_together = "\n\n{}\n\n".format(15 * msg_underline).join(
         [str(rw.message) for rw in recorded_warns]
     )
-    warnings_string = "Warnings\n{}\n{}".format(header_underline, warnings_together)
-    failures_string = "Failures\n========\n{}".format(msg_underline.join(failures))
-    output_string = "{}\n\n{}".format(failures_string, warnings_string)
+
+    warnings_header = "Warnings"
+    warnings_string = "{}\n{}\n{}".format(
+        warnings_header, len(warnings_header) * header_underline, warnings_together
+    )
+
+    failures_header = "Failures"
+    failures_string = "{}\n{}\n{}".format(
+        failures_header,
+        len(failures_header) * header_underline,
+        msg_underline.join(failures),
+    )
+
+    already_exist_header = "Already exist"
+    if already_exist_files:
+        already_exist_files_string = "- {}\n".format("\n- ".join(already_exist_files))
+    else:
+        already_exist_files_string = ""
+    already_exist_string = "{}\n{}\n{}".format(
+        already_exist_header,
+        len(already_exist_header) * header_underline,
+        already_exist_files_string,
+    )
+
+    metadata_header = (
+        "Files crunched with NetCDF SCM\ntimestamp: {}\n\n"
+        "NetCDF SCM version: {}\n"
+        "input: {}\n"
+        "output: {}\n"
+        "var-to-crunch: {}\n"
+        "land-mask-threshold: {}\n"
+        "force: {}\n".format(
+            timestamp,
+            netcdf_scm.__version__,
+            in_dir,
+            out_dir,
+            var_to_crunch,
+            land_mask_threshold,
+            force_regeneration,
+        )
+    )
+    output_string = "{}\n\n{}\n\n{}\n\n{}".format(
+        metadata_header, failures_string, warnings_string, already_exist_string
+    )
     print(output_string)
-    with open(
-        join(OUTPUT_DIR, "..", "{}_failures_and_warnings.txt".format(output_prefix)),
-        "w",
-    ) as ef:
+    summary_file = join(
+        out_dir, separator.join([out_sub_dir, "failures-and-warnings.txt"])
+    )
+    with open(summary_file, "w") as ef:
         ef.write(output_string)
 
 
-crunch_data(
-    INPUT_DIR,
-    OUTPUT_DIR,
-    var_to_crunch=VAR_TO_CRUNCH,
-    land_mask_threshold=LAND_MASK_THRESHOLD,
-    force_regeneration=True,
-    output_prefix="scm_crunched",
-)
+def main():
+    parser = argparse.ArgumentParser(
+        prog="crunch-to-netcdf-scm",
+        description="Crunch netCDF files to NetCDF SCM csv files",
+    )
+
+    # Command line args
+    parser.add_argument("input", help="Root folder of the data to crunch.")
+    parser.add_argument("output", help="Root folder in which to save the output data")
+    parser.add_argument("--var-to-crunch", help="Variable to crunch", default=None)
+    parser.add_argument(
+        "--land-mask-threshold",
+        help="Minimum fraction of a box which must be land for it to be counted as a land box",
+        default=50,
+        type=float,
+    )
+    parser.add_argument(
+        "-f", "--force", help="Overwrite any existing files", action="store_true"
+    )
+
+    args = parser.parse_args()
+
+    crunch_data(
+        args.input,
+        args.output,
+        var_to_crunch=args.var_to_crunch,
+        land_mask_threshold=args.land_mask_threshold,
+        force_regeneration=args.force,
+    )
+
+
+if __name__ == "__main__":
+    main()
