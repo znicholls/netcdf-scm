69889798edd9d0d89ac5991939d0e18d24ae62f4
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2019-05-08 10:36:00 +1000

    Tidy up setup py file

--- setup.py
+++ setup.py
@@ -5,47 +5,72 @@ from setuptools.command.test import test as TestCommand
 
 
 PACKAGE_NAME = "netcdf-scm"
+DESCRIPTION = "Processing netCDF files for use with simple climate models"
+KEYWORDS = [
+    "netcdf",
+    "netCDF",
+    "python",
+    "climate",
+    "atmosphere",
+    "simple climate model",
+    "reduced complexity climate model",
+    "data processing",
+]
+
 AUTHOR = "Zebedee Nicholls"
 EMAIL = "zebedee.nicholls@climate-energy-college.org"
 URL = "https://github.com/znicholls/netcdf-scm"
+PROJECT_URLS = {
+    "Bug Reports": "https://github.com/znicholls/netcdf-scm/issues",
+    "Documentation": "https://openscm.readthedocs.io/en/latest",
+    "Source": "https://github.com/znicholls/netcdf-scm",
+}
+LICENSE = "2-Clause BSD License"
+CLASSIFIERS = [
+    "Development Status :: 4 - Beta",
+    "License :: OSI Approved :: BSD License",
+    "Intended Audience :: Developers",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python :: 3.7",
+]
 
-DESCRIPTION = (
-    "Python wrapper for processing netCDF files for use with simple climate models"
-)
-README = "README.rst"
-
-SOURCE_DIR = "src"
-
-REQUIREMENTS = [
+REQUIREMENTS_INSTALL = [
     "numpy",
     "pandas",
     "python-dateutil",
     "progressbar2",
     "expectexception",
     "openscm>=0.1.0a",
+    "click",
 ]
-REQUIREMENTS_TESTS = ["codecov", "pytest-cov", "pytest>=4.0", "notebook", "nbval"]
+REQUIREMENTS_TESTS = ["codecov", "pytest-cov", "pytest>=4.0"]
+REQUIREMENTS_NOTEBOOKS = [
+    # TODO use pypi version
+    "pyam-iamc @ git+https://github.com/IAMconsortium/pyam.git@a6ac0c574c3d5363fb8dba7d8b42a845fd3dbe87",
+    "notebook",
+    "nbval"
+]
 REQUIREMENTS_DOCS = ["sphinx>=1.4", "sphinx_rtd_theme"]
 REQUIREMENTS_DEPLOY = ["twine>=1.11.0", "setuptools>=38.6.0", "wheel>=0.31.0"]
-
 requirements_dev = [
-    *[
-        "flake8",
-        "black",
-        # TODO use pypi version
-        "pyam-iamc @ git+https://github.com/IAMconsortium/pyam.git@a6ac0c574c3d5363fb8dba7d8b42a845fd3dbe87",
-    ],
+    *["flake8", "black"],
     *REQUIREMENTS_TESTS,
+    *REQUIREMENTS_NOTEBOOKS,
     *REQUIREMENTS_DOCS,
     *REQUIREMENTS_DEPLOY,
 ]
 
-requirements_extras = {
+REQUIREMENTS_EXTRAS = {
     "docs": REQUIREMENTS_DOCS,
     "tests": REQUIREMENTS_TESTS,
+    "notebooks": REQUIREMENTS_NOTEBOOKS,
     "deploy": REQUIREMENTS_DEPLOY,
     "dev": requirements_dev,
 }
+
+SOURCE_DIR = "src"
+
+README = "README.rst"
 
 with open(README, "r") as readme_file:
     README_TEXT = readme_file.read()
@@ -75,26 +100,14 @@ setup(
     author=AUTHOR,
     author_email=EMAIL,
     url=URL,
-    license="2-Clause BSD License",
-    classifiers=[
-        "Development Status :: 4 - Beta",
-        "License :: OSI Approved :: BSD License",
-        "Intended Audience :: Developers",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python :: 3.7",
-    ],
-    keywords=[
-        "netcdf",
-        "python",
-        "climate",
-        "atmosphere",
-        "simple climate model",
-        "reduced complexity climate model",
-    ],
+    project_urls=PROJECT_URLS,
+    license=LICENSE,
+    classifiers=CLASSIFIERS,
+    keywords=KEYWORDS,
     packages=find_packages(SOURCE_DIR),  # no tests/docs in `src` so don't need exclude
     package_dir={"": SOURCE_DIR},
     # include_package_data=True,
-    install_requires=REQUIREMENTS,
-    extras_require=requirements_extras,
+    install_requires=REQUIREMENTS_INSTALL,
+    extras_require=REQUIREMENTS_EXTRAS,
     cmdclass=cmdclass,
 )
