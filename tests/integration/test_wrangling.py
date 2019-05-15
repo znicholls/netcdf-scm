8c1bdf84239cc585cc9984357380524fa445474f
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2019-05-15 17:12:48 +1000

    Pass test of wrangilng
    Former-commit-id: e0c300879a7317ab12f38b2c83be40274f384815

--- tests/integration/test_wrangling.py
+++ tests/integration/test_wrangling.py
@@ -1,6 +1,19 @@
+from os import listdir
+from os.path import isdir, join
+
+from click.testing import CliRunner
+
+
+import netcdf_scm
+from netcdf_scm.cli import wrangle_openscm_csvs
+
+
+
+from conftest import TEST_DATA_OPENSCMCSVS_DIR
+
 def test_wrangling_defaults(tmpdir):
     INPUT_DIR = TEST_DATA_OPENSCMCSVS_DIR
-    OUTPUT_DIR = str(tmpdir)
+    OUTPUT_DIR = str(join(tmpdir, "new-sub-dir"))
 
     runner = CliRunner()
     result = runner.invoke(
@@ -18,10 +31,10 @@ def test_wrangling_defaults(tmpdir):
     assert "rsut" in result.output
     assert "rlut" in result.output
 
-    assert isdir(join(OUTPUT_DIR, "CMIP6", "one model"))
-    assert isdir(join(OUTPUT_DIR, "CMIP6", "other model"))
-    assert isdir(join(OUTPUT_DIR, "CMIP6", "third dir"))
-    assert isdir(join(OUTPUT_DIR, "CMIP6", "fourth dir"))
+    assert isdir(join(OUTPUT_DIR, "cmip6/CMIP6/CMIP/BCC/BCC-CSM2-MR/1pctCO2/r1i1p1f1/Amon/rlut"))
+    assert isdir(join(OUTPUT_DIR, "cmip6/CMIP6/CMIP/BCC/BCC-CSM2-MR/1pctCO2/r1i1p1f1/Amon/rsut"))
+    assert isdir(join(OUTPUT_DIR, "cmip6/CMIP6/ScenarioMIP/IPSL"))
+    assert isdir(join(OUTPUT_DIR, "cmip6/CMIP6/ScenarioMIP/MRI"))
 
 
 def test_wrangling_var(tmpdir):
@@ -35,22 +48,22 @@ def test_wrangling_var(tmpdir):
         [
             INPUT_DIR,
             OUTPUT_DIR,
-            "--variable",
+            "--var-to-wrangle",
             VAR_TO_WRANGLE,
             "--nested",
+            "--out-format",
+            "tuningstrucs",
         ],
     )
     assert result.exit_code == 0
 
     assert "NetCDF SCM version: {}".format(netcdf_scm.__version__) in result.output
-    assert "Making output directory:" in result.output
 
     assert "rsut" in result.output
     assert "rlut" not in result.output
 
-
-    assert isdir(join(OUTPUT_DIR, "CMIP6", "one model"))
-    assert isdir(join(OUTPUT_DIR, "CMIP6", "other model"))
+    assert isdir(join(OUTPUT_DIR, "cmip6/CMIP6/CMIP/BCC/BCC-CSM2-MR/1pctCO2/r1i1p1f1/Amon/rsut"))
+    assert isdir(join(OUTPUT_DIR, "cmip6/CMIP6/ScenarioMIP/MRI/MRI-ESM2-0/ssp126/r1i1p1f1/Amon/rsut"))
 
 
 def test_wrangling_flat(tmpdir):
@@ -69,9 +82,7 @@ def test_wrangling_flat(tmpdir):
     assert result.exit_code == 0
 
     assert "NetCDF SCM version: {}".format(netcdf_scm.__version__) in result.output
-    assert "Making output directory:" in result.output
 
-    assert "rsut" in result.output
-    assert "rlut" in result.output
+    assert "everything" in result.output
 
     assert len(listdir(OUTPUT_DIR)) == 3
