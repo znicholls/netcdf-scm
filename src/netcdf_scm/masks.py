23e08eae371c29a36e20b798762c27ac9b9974b9
Jared Lewis <jared@jared.kiwi.nz>
2019-05-17 13:56:20 +1000

    Added CubeMasker object to perform the masking
    Former-commit-id: ca10f7ce1dc4400655a88e41f045c654435e3576

--- src/netcdf_scm/masks.py
+++ src/netcdf_scm/masks.py
@@ -9,15 +9,20 @@ except ModuleNotFoundError:
     raise_no_iris_warning()
 
 import numpy as np
+import warnings
 
-DEFAULT_REGIONS = [
+DEFAULT_REGIONS = (
+    "World",
+    "World|Land",
+    "World|Ocean",
+    "World|Northern Hemisphere",
+    "World|Southern Hemisphere",
     "World|Northern Hemisphere|Land",
     "World|Southern Hemisphere|Land",
     "World|Northern Hemisphere|Ocean",
     "World|Southern Hemisphere|Ocean",
-    "World|Land",
-    "World|Ocean",
-]
+
+)
 
 
 def broadcast_onto_lat_lon_grid(cube, array_in):
@@ -47,44 +52,104 @@ def broadcast_onto_lat_lon_grid(cube, array_in):
     return broadcast_to_shape(array_in, cube.cube.shape, dim_order)
 
 
-def get_land_mask(cube, sftlf_cube=None, land_mask_threshold=50):
-    """Get the land mask.
-
-    Returns
-    -------
-    np.ndarray
-    """
-    # Lazy loaded to avoid cyclic dependency
-    from .iris_cube_wrappers import SCMCube
-    if sftlf_cube is None:
-        sftlf_cube = cube.get_metadata_cube(cube.sftlf_var)
-
-    if not isinstance(sftlf_cube, SCMCube):
-        raise TypeError("sftlf_cube must be an SCMCube instance")
-
-    sftlf_data = sftlf_cube.cube.data
-
-    land_mask = np.where(
-        sftlf_data > land_mask_threshold,
-        False,  # where it's land, return False i.e. don't mask
-        True,  # otherwise True
-    )
-
-    return broadcast_onto_lat_lon_grid(cube, land_mask)
+class InvalidMask(Exception):
+    pass
 
 
-def get_nh_mask(cube):
-    mask_nh_lat = np.array(
-        [cell < 0 for cell in cube.cube.coord(cube.lat_name).cells()]
-    )
-    mask_all_lon = np.full(cube.cube.coord(cube.lon_name).points.shape, False)
+class CubeMasker:
 
-    # Here we make a grid which we can use as a mask. We have to use all
-    # of these nots so that our product (which uses AND logic) gives us
-    # False in the NH and True in the SH (another way to think of this is
-    # that we have to flip everything so False goes to True and True goes
-    # to False, do all our operations with AND logic, then flip everything
-    # back).
-    mask_nh = ~np.outer(~mask_nh_lat, ~mask_all_lon)
+    def __init__(self, cube, **kwargs):
+        self.cube = cube
+        self._masks = {}
+        self.kwargs = kwargs
 
-    return broadcast_onto_lat_lon_grid(cube, mask_nh)
+        self._mask_funcs = {
+            "World": lambda: np.full(self.get_mask("World|Northern Hemisphere").shape, False),
+            "World|Northern Hemisphere": self.get_nh_mask,
+            "World|Southern Hemisphere": lambda: ~self.get_mask('World|Northern Hemisphere'),
+            "World|Land": lambda: self.get_land_mask(**self.kwargs),
+            "World|Ocean": lambda: ~self.get_mask('World|Land'),
+            "World|Northern Hemisphere|Land": lambda: self.get_hemi_mask("World|Northern Hemisphere", "World|Land"),
+            "World|Southern Hemisphere|Land": lambda: self.get_hemi_mask("World|Southern Hemisphere", "World|Land"),
+            "World|Northern Hemisphere|Ocean": lambda: self.get_hemi_mask("World|Northern Hemisphere", "World|Ocean"),
+            "World|Southern Hemisphere|Ocean": lambda: self.get_hemi_mask("World|Southern Hemisphere", "World|Ocean"),
+        }
+
+    def get_mask(self, mask_name):
+        try:
+            return self._masks[mask_name]
+        except KeyError:
+            try:
+                mask = self._mask_funcs[mask_name]()
+                self._masks[mask_name] = mask
+            except KeyError:
+                raise InvalidMask('Unknown mask: {}'.format(mask_name))
+            except InvalidMask:
+                raise
+
+        return mask
+
+    def get_masks(self, mask_names):
+        masks = {}
+        for name in mask_names:
+            try:
+                mask = self.get_mask(name)
+                masks[name] = mask
+            except InvalidMask:
+                pass
+        return masks
+
+    def get_hemi_mask(self, hemi=None, surface=None):
+        hemi = self.get_mask(hemi) if hemi is not None else True
+        surface = self.get_mask(surface) if surface is not None else True
+
+        return np.logical_or(hemi, surface)
+
+    def get_land_mask(self, sftlf_cube=None, land_mask_threshold=50, **kwargs):
+        """Get the land mask.
+
+        Returns
+        -------
+        np.ndarray
+        """
+        # Lazy loaded to avoid cyclic dependency
+        from .iris_cube_wrappers import SCMCube
+        if sftlf_cube is None:
+            try:
+                sftlf_cube = self.cube.get_metadata_cube(self.cube.sftlf_var)
+            except OSError:
+                warn_msg = (
+                    "Land surface fraction (sftlf) data not available, only returning "
+                    "global and hemispheric masks."
+                )
+                warnings.warn(warn_msg)
+                raise InvalidMask()
+
+        if not isinstance(sftlf_cube, SCMCube):
+            raise TypeError("sftlf_cube must be an SCMCube instance")
+
+        sftlf_data = sftlf_cube.cube.data
+
+        land_mask = np.where(
+            sftlf_data > land_mask_threshold,
+            False,  # where it's land, return False i.e. don't mask
+            True,  # otherwise True
+        )
+
+        return broadcast_onto_lat_lon_grid(self.cube, land_mask)
+
+    def get_nh_mask(self):
+        mask_nh_lat = np.array(
+            [cell < 0 for cell in self.cube.cube.coord(self.cube.lat_name).cells()]
+        )
+        mask_all_lon = np.full(self.cube.cube.coord(self.cube.lon_name).points.shape, False)
+
+        # Here we make a grid which we can use as a mask. We have to use all
+        # of these nots so that our product (which uses AND logic) gives us
+        # False in the NH and True in the SH (another way to think of this is
+        # that we have to flip everything so False goes to True and True goes
+        # to False, do all our operations with AND logic, then flip everything
+        # back).
+        mask_nh = ~np.outer(~mask_nh_lat, ~mask_all_lon)
+
+        return broadcast_onto_lat_lon_grid(self.cube, mask_nh)
---
+++ tests/unit/test_masks.py
@@ -0,0 +1,205 @@
+import re
+import warnings
+from unittest.mock import patch, MagicMock
+
+import iris
+import numpy as np
+import pytest
+from iris.util import broadcast_to_shape
+
+from netcdf_scm.masks import CubeMasker, DEFAULT_REGIONS, InvalidMask
+
+from conftest import create_sftlf_cube
+
+
+@patch.object(CubeMasker, 'get_land_mask')
+@patch.object(CubeMasker, 'get_nh_mask')
+def test_get_scm_masks(mock_nh_mask, mock_land_mask, test_all_cubes):
+    tsftlf_cube = "mocked 124"
+    tland_mask_threshold = "mocked 51"
+
+    land_mask = np.array(
+        [
+            [False, True, True, False],
+            [False, True, False, True],
+            [False, False, True, False],
+        ]
+    )
+    mock_land_mask.return_value = land_mask
+
+    nh_mask = np.array(
+        [
+            [False, False, False, False],
+            [False, False, False, False],
+            [True, True, True, True],
+        ]
+    )
+    mock_nh_mask.return_value = nh_mask
+
+    nh_land_mask = np.array(
+        [
+            [False, True, True, False],
+            [False, True, False, True],
+            [True, True, True, True],
+        ]
+    )
+    # check our logic while we're here
+    np.testing.assert_array_equal(np.logical_or(nh_mask, land_mask), nh_land_mask)
+
+    expected = {
+        "World": np.full(nh_mask.shape, False),
+        "World|Northern Hemisphere|Land": nh_land_mask,
+        "World|Southern Hemisphere|Land": np.logical_or(~nh_mask, land_mask),
+        "World|Northern Hemisphere|Ocean": np.logical_or(nh_mask, ~land_mask),
+        "World|Southern Hemisphere|Ocean": np.logical_or(~nh_mask, ~land_mask),
+        "World|Land": land_mask,
+        "World|Ocean": ~land_mask,
+        "World|Northern Hemisphere": nh_mask,
+        "World|Southern Hemisphere": ~nh_mask,
+    }
+
+    masker = CubeMasker(test_all_cubes, sftlf_cube=tsftlf_cube, land_mask_threshold=tland_mask_threshold)
+    result = masker.get_masks(DEFAULT_REGIONS)
+
+    for label, array in expected.items():
+        np.testing.assert_array_equal(array, result[label])
+    mock_land_mask.assert_called_with(sftlf_cube=tsftlf_cube, land_mask_threshold=tland_mask_threshold)
+    mock_nh_mask.assert_called_with()
+
+
+@patch.object(CubeMasker, 'get_nh_mask')
+def test_get_scm_masks_no_land_available(mock_nh_mask, test_all_cubes):
+    test_all_cubes.get_metadata_cube = MagicMock(side_effect=OSError)
+
+    nh_mask = np.array(
+        [
+            [False, False, False, False],
+            [False, False, False, False],
+            [True, True, True, True],
+        ]
+    )
+    mock_nh_mask.return_value = nh_mask
+
+    expected = {
+        "World": np.full(nh_mask.shape, False),
+        "World|Northern Hemisphere": nh_mask,
+        "World|Southern Hemisphere": ~nh_mask,
+    }
+    expected_warn = (
+        "Land surface fraction (sftlf) data not available, only returning "
+        "global and hemispheric masks."
+    )
+    masker = CubeMasker(test_all_cubes)
+    with warnings.catch_warnings(record=True) as no_sftlf_warns:
+        result = masker.get_masks(DEFAULT_REGIONS)
+
+    assert len(no_sftlf_warns) == 1
+    assert str(no_sftlf_warns[0].message) == expected_warn
+
+    for label, array in expected.items():
+        np.testing.assert_array_equal(array, result[label])
+    mock_nh_mask.assert_called_with()
+
+
+@pytest.mark.parametrize("transpose", [True, False])
+@pytest.mark.parametrize("input_format", ["scmcube", None])
+@pytest.mark.parametrize("sftlf_var", ["sftlf", "sftlf_other"])
+@pytest.mark.parametrize(
+    "test_threshold",
+    [(None), (0), (10), (30), (49), (49.9), (50), (50.1), (51), (60), (75), (100)],
+)
+def test_get_land_mask(
+        test_all_cubes,
+        test_threshold,
+        input_format,
+        sftlf_var,
+        transpose,
+):
+    sftlf_cube = create_sftlf_cube(test_all_cubes.__class__)
+    test_all_cubes.sftlf_var = sftlf_var
+    original_data = sftlf_cube.cube.data
+
+    if transpose:
+        sftlf_cube.cube = iris.cube.Cube(
+            data=np.transpose(sftlf_cube.cube.data)
+        )
+    test_all_cubes.get_metadata_cube = MagicMock(return_value=sftlf_cube)
+
+    test_land_fraction_input = (
+        sftlf_cube if input_format == "scmcube" else None
+    )
+
+    masker = CubeMasker(test_all_cubes)
+    if test_threshold is None:
+        result = masker.get_land_mask(sftlf_cube=test_land_fraction_input)
+        # test that default land fraction is 50%
+        test_threshold = 50
+    else:
+        result = masker.get_land_mask(
+            test_land_fraction_input, land_mask_threshold=test_threshold
+        )
+
+    # having got the result, we can now update test_land_fraction_input
+    # for our assertions
+    if test_land_fraction_input is None:
+        test_all_cubes.get_metadata_cube.assert_called_with(test_all_cubes.sftlf_var)
+        test_land_fraction_input = sftlf_cube
+
+    # where it's land return False, otherwise True to match with masking
+    # convention that True means masked
+    expected = broadcast_to_shape(
+        np.where(original_data > test_threshold, False, True),
+        test_all_cubes.cube.shape,
+        [test_all_cubes.lat_dim_number, test_all_cubes.lon_dim_number],
+    )
+    np.testing.assert_array_equal(result, expected)
+
+    if input_format is None:
+        test_all_cubes.get_metadata_cube.assert_called_with(sftlf_var)
+    else:
+        test_all_cubes.get_metadata_cube.assert_not_called()
+
+
+@pytest.mark.parametrize("input", ["fail string", np.array([[1, 2], [3, 4]])])
+def test_get_land_mask_input_type_errors(test_all_cubes, input):
+    error_msg = re.escape(r"sftlf_cube must be an SCMCube instance")
+    masker = CubeMasker(test_all_cubes)
+    with pytest.raises(TypeError, match=error_msg):
+        masker.get_land_mask(sftlf_cube=input)
+
+
+def test_get_land_mask_shape_errors(test_all_cubes):
+    sftlf_cube = create_sftlf_cube(test_all_cubes.__class__)
+    error_msg = re.escape(
+        r"the sftlf_cube data must be the same shape as the "
+        r"cube's longitude-latitude grid"
+    )
+
+    wrong_shape_data = np.array([[1, 2], [3, 4]])
+    sftlf_cube.cube = iris.cube.Cube(data=wrong_shape_data)
+    masker = CubeMasker(test_all_cubes)
+    with pytest.raises(AssertionError, match=error_msg):
+        masker.get_land_mask(sftlf_cube=sftlf_cube)
+
+    test_all_cubes.get_metadata_cube = MagicMock(return_value=sftlf_cube)
+    with pytest.raises(AssertionError, match=error_msg):
+        masker.get_land_mask(sftlf_cube=None)
+
+
+def test_get_nh_mask(test_all_cubes):
+    masker = CubeMasker(test_all_cubes)
+    result = masker.get_nh_mask()
+    expected_base = np.array(
+        [
+            [False, False, False, False],
+            [False, False, False, False],
+            [True, True, True, True],
+        ]
+    )
+    expected = broadcast_to_shape(
+        expected_base,
+        test_all_cubes.cube.shape,
+        [test_all_cubes.lat_dim_number, test_all_cubes.lon_dim_number],
+    )
+
+    np.testing.assert_array_equal(result, expected)
