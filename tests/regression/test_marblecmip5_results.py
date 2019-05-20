7203f806e4e0b25e0a90ee4cebd94b76bc462adb
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2019-05-20 11:02:00 +1000

    Add marble crunching regression test
    Former-commit-id: bdeeed61f6f039d6ed5658b16d3857d4c5bcbabe

---
+++ tests/regression/test_marblecmip5_results.py
@@ -0,0 +1,39 @@
+from os.path import join
+
+
+from click.testing import CliRunner
+
+
+from netcdf_scm.cli import crunch_data
+
+
+from conftest import TEST_DATA_ROOT_DIR, TEST_DATA_MARBLE_CMIP5_DIR, run_crunching_comparison
+
+
+EXPECTED_FILES_DIR = join(TEST_DATA_ROOT_DIR, "expected-crunching-output")
+
+
+def test_crunching(tmpdir, update_expected_file):
+    INPUT_DIR = TEST_DATA_MARBLE_CMIP5_DIR
+    OUTPUT_DIR = str(tmpdir)
+    VAR_TO_CRUNCH = ".*tas.*"
+
+    runner = CliRunner()
+    result = runner.invoke(
+        crunch_data,
+        [
+            INPUT_DIR,
+            OUTPUT_DIR,
+            "--cube-type",
+            "MarbleCMIP5",
+            "--regexp",
+            VAR_TO_CRUNCH,
+            "-f",
+        ],
+    )
+    assert result.exit_code == 0
+    run_crunching_comparison(
+        join(OUTPUT_DIR, "netcdf-scm-crunched", "cmip5"),
+        join(EXPECTED_FILES_DIR, "marble-cmip5", "cmip5"),
+        update=update_expected_file,
+    )
