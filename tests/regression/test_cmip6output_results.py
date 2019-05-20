97197b336938d3b2aa4ba68e66c5f7c1263de9a1
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2019-05-20 11:23:23 +1000

    Add CMIP6Output crunching regression tests
    Former-commit-id: e9951b915ed2f38c532c8217d540c190965589cc

---
+++ tests/regression/test_cmip6output_results.py
@@ -0,0 +1,36 @@
+from os.path import join
+
+
+from click.testing import CliRunner
+
+
+from netcdf_scm.cli import crunch_data
+
+
+from conftest import TEST_DATA_ROOT_DIR, TEST_DATA_CMIP6Output_DIR, run_crunching_comparison
+
+
+EXPECTED_FILES_DIR = join(TEST_DATA_ROOT_DIR, "expected-crunching-output")
+
+
+def test_crunching(tmpdir, update_expected_files):
+    INPUT_DIR = TEST_DATA_CMIP6Output_DIR
+    OUTPUT_DIR = str(tmpdir)
+
+    runner = CliRunner()
+    result = runner.invoke(
+        crunch_data,
+        [
+            INPUT_DIR,
+            OUTPUT_DIR,
+            "--cube-type",
+            "CMIP6Output",
+            "-f",
+        ],
+    )
+    assert result.exit_code == 0, result.output
+    run_crunching_comparison(
+        join(OUTPUT_DIR, "netcdf-scm-crunched", "CMIP6"),
+        join(EXPECTED_FILES_DIR, "cmip6output", "CMIP6"),
+        update=update_expected_files,
+    )
