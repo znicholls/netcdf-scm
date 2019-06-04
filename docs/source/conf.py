d98cfb04bab6e5f6399299c8892f9b710d0b39ad
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2019-06-04 10:07:37 +1000

    Add hotfix

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -57,13 +57,21 @@ extensions = [
     "sphinx_click.ext",
 ]
 
-autodoc_default_options = {
-    "members": True,
-    "undoc-members": True,
-    "private-members": False,
-    "inherited-members": True,
-    "show-inheritance": True,
-}
+autodoc_default_flags = [
+    "members",
+    # "undoc-members",
+    # "private-members",
+    "inherited-members",
+    "show-inheritance",
+]
+# change to below when upgrading to Sphinx >= 2.1
+# autodoc_default_options = {
+#     "undoc-members": False,
+#     "private-members": False,
+#     "special-members": False,
+#     "inherited-members": False,
+#     "show-inheritance": False,
+# }
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
