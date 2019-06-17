6bdf96093e023faf2d7befe24e0e14d7d040806c
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2019-06-17 16:01:35 +1000

    Remove progressbar usage and fix logging levels
    Former-commit-id: 8de76d19ce296ce0ef27bc07c000ea92415da786

--- src/netcdf_scm/masks/__init__.py
+++ src/netcdf_scm/masks/__init__.py
@@ -115,19 +115,21 @@ def get_land_mask(masker, cube, sftlf_cube=None, land_mask_threshold=50, **kwarg
     np.ndarray
     """
     warn_msg = "Land surface fraction (sftlf) data not available, using default instead"
-    try:
-        sftlf_cube = cube.get_metadata_cube(cube.sftlf_var, cube=sftlf_cube)
-        sftlf_data = sftlf_cube.cube.data
-    except OSError:
-        logger.warning(warn_msg)
-        def_cube_regridded = get_default_sftlf_cube().regrid(
-            cube.cube, iris.analysis.Linear()
-        )
-        sftlf_data = def_cube_regridded.data
     if sftlf_cube is None:
-        logger.warning(warn_msg)
-        sftlf_cube = get_default_sftlf_cube().regrid(cube.cube, iris.analysis.Linear())
-        sftlf_data = def_cube_regridded.data
+        try:
+            sftlf_cube = cube.get_metadata_cube(cube.sftlf_var, cube=sftlf_cube)
+            sftlf_data = sftlf_cube.cube.data
+        except OSError:
+            logger.warning(warn_msg)
+            def_cube_regridded = get_default_sftlf_cube().regrid(
+                cube.cube, iris.analysis.Linear()
+            )
+            sftlf_data = def_cube_regridded.data
+        # logger.warning(warn_msg)
+        # sftlf_cube = get_default_sftlf_cube().regrid(cube.cube, iris.analysis.Linear())
+        # sftlf_data = def_cube_regridded.data
+    else:
+        sftlf_data = sftlf_cube.cube.data
 
     land_mask = np.where(
         sftlf_data > land_mask_threshold,
