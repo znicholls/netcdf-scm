df544b8822c00611de5e1d34ad590b1a7b1a0717
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2019-06-20 14:48:16 +1000

    Start doing codacy hot fixes and make CI output more useful

--- scripts/test_install.py
+++ scripts/test_install.py
@@ -11,7 +11,7 @@ import netcdf_scm
 def import_submodules(package_name):
     package = importlib.import_module(package_name)
 
-    for loader, name, is_pkg in pkgutil.walk_packages(package.__path__):
+    for _, name, is_pkg in pkgutil.walk_packages(package.__path__):
         full_name = package.__name__ + "." + name
         importlib.import_module(full_name)
         if is_pkg:
