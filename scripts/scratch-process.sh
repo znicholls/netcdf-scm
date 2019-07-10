78dd8f86c761b590a16aca838c0f1798c42a6e24
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2019-07-10 12:15:10 +1000

    Make comparisons a fair test

--- scripts/scratch-process.sh
+++ scripts/scratch-process.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 SRC_DIR="/media/research-nfs/cmip6/CMIP6/CMIP/IPSL/IPSL-CM6A-LR/1pctCO2/r1i1p1f1/Amon/"
-#SRC_DIR="/data/marble/cmip6/CMIP6/CMIP/IPSL/IPSL-CM6A-LR/1pctCO2/r1i1p1f1/Amon/"
+SRC_DIR="/data/marble/cmip6/CMIP6/CMIP/IPSL/IPSL-CM6A-LR/1pctCO2/r1i1p1f1/Amon/"
 CRUNCH_DIR="/data/marble/sandbox/share/cmip6-crunched-ipsl-sandbox"
 WRANGLE_DIR="/data/marble/sandbox/share/cmip6-wrangled-ipsl-sandbox"
 CONTACT='serial test Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>, Jared Lewis <jared.lewis@climate-energy-college.org>, Malte Meinshausen <malte.meinshausen@unimelb.edu.au>'
 DRS="CMIP6Output"
 #REGEXP="^(?!.*fx).*IPSL.*$"
-REGEXP="^(?!.*fx).*$"
+REGEXP="^(?!.*(fx|/ta/)).*$"
 # have to be super careful when crunching input files as the duplicate grids can cause
 # things to explode
 REGEXP_WRANGLE_IN_FILES=".*tas/.*gr/.*"
