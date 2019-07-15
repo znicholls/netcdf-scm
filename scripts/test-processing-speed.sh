6d7539893c6e8b44c19f10b9958c5bdf0e52b9da
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2019-07-15 21:05:54 +1000

    Profile code and do speed test
    Former-commit-id: e1cdec0d5ae9ecd9009af5e279443402d50520b8

---
+++ scripts/test-processing-speed.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+SRC_DIR="/data/marble/cmip6/CMIP6/DCPP/IPSL/IPSL-CM6A-LR/dcppC-atl-control"
+CRUNCH_DIR="/data/marble/sandbox/share/cmip6-crunched-ipsl-sandbox-speed-test"
+CONTACT='Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>, Jared Lewis <jared.lewis@climate-energy-college.org>, Malte Meinshausen <malte.meinshausen@unimelb.edu.au>'
+DRS="CMIP6Output"
+#REGEXP="^(?!.*(fx|ta/)).*(tas/|fco2nat|rsut|rlut|rsdt).*$"
+REGEXP="^(?!.*(fx|/ta/|/co2/)).*Amon.*$"
+
+exec 3>&1 4>&2
+SRC_DIR_TIME=$( { time netcdf-scm-crunch "${SRC_DIR}" "${CRUNCH_DIR}" "${CONTACT}"  --drs "${DRS}" --regexp "${REGEXP}" --force 1>&3 2>&4; } 2>&1 )
+exec 3>&- 4>&-
+
+
+SRC_DIR_MEDIUM="/data/marble/cmip6/CMIP6/CMIP/BCC/BCC-CSM2-MR/historical"
+
+exec 3>&1 4>&2
+SRC_DIR_MEDIUM_TIME=$( { time netcdf-scm-crunch "${SRC_DIR_MEDIUM}" "${CRUNCH_DIR}" "${CONTACT}"  --drs "${DRS}" --regexp "${REGEXP}" --force 1>&3 2>&4; } 2>&1 )
+exec 3>&- 4>&-
+
+
+SRC_DIR_BIG="/data/marble/cmip6/CMIP6/CMIP/BCC/BCC-CSM2-MR/piControl/r1i1p1f1/Amon"
+
+exec 3>&1 4>&2
+SRC_DIR_BIG_TIME=$( { time netcdf-scm-crunch "${SRC_DIR_BIG}" "${CRUNCH_DIR}" "${CONTACT}"  --drs "${DRS}" --regexp "${REGEXP}" --force 1>&3 2>&4; } 2>&1 )
+exec 3>&- 4>&-
+
+exec 3>&1 4>&2
+VERSION=$( { python -c "import netcdf_scm; import click; click.echo('NetCDF-SCM version: {}'.format(netcdf_scm.__version__))" 1>&3 2>&4; } 2>&1 )
+exec 3>&- 4>&-
+
+echo "NetCDF-SCM version: $VERSION"
+echo "Multiple small file (10 yrs) time: $SRC_DIR_TIME"
+echo "Multiple medium file (250 yrs) crunch time: $SRC_DIR_MEDIUM_TIME"
+echo "Multiple big file (750 yrs) crunch time: $SRC_DIR_BIG_TIME"
+
