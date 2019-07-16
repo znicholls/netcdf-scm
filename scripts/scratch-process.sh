882fa150047b668ae6ae8551535a53153c0f44c3
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2019-07-16 18:29:10 +1000

    Make scratch process script sensible
    Former-commit-id: 5626febcefa69b8422de4c66c8b293aa5a38b1e8

--- scripts/scratch-process.sh
+++ scripts/scratch-process.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-SRC_DIR="/media/research-nfs/cmip6/"
-CRUNCH_DIR="/data/marble/sandbox/share/cmip6-crunched-ipsl-sandbox"
 SRC_DIR="tests/test-data/cmip6output"
 CRUNCH_DIR="output-examples/scratch-process-output"
-WRANGLE_DIR="/data/marble/sandbox/share/cmip6-wrangled-ipsl-sandbox"
+WRANGLE_DIR="output-examples/wrangle-process-output"
 CONTACT='Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>, Jared Lewis <jared.lewis@climate-energy-college.org>, Malte Meinshausen <malte.meinshausen@unimelb.edu.au>'
 DRS="CMIP6Output"
 REGEXP="^(?!.*fx).*IPSL.*$"
@@ -11,6 +9,6 @@ REGEXP="^(?!.*fx).*IPSL.*$"
 # things to explode
 REGEXP_WRANGLE_IN_FILES=".*tas/.*gr/.*"
 
-netcdf-scm-crunch "${SRC_DIR}" "${CRUNCH_DIR}" "${CONTACT}"  --drs "${DRS}" --regexp "${REGEXP}"
-netcdf-scm-wrangle "${CRUNCH_DIR}/netcdf-scm-crunched/CMIP6/" "${WRANGLE_DIR}" "${CONTACT}" --drs "${DRS}"
-netcdf-scm-wrangle "${CRUNCH_DIR}/netcdf-scm-crunched/CMIP6/" "${WRANGLE_DIR}" "${CONTACT}" --drs "${DRS}" --out-format magicc-input-files-point-end-of-year --regexp "${REGEXP_WRANGLE_IN_FILES}"
+netcdf-scm-crunch "${SRC_DIR}" "${CRUNCH_DIR}" "${CONTACT}"  --drs "${DRS}" --regexp "${REGEXP}" -f
+netcdf-scm-wrangle "${CRUNCH_DIR}/netcdf-scm-crunched/CMIP6/" "${WRANGLE_DIR}" "${CONTACT}" --drs "${DRS}" -f
+netcdf-scm-wrangle "${CRUNCH_DIR}/netcdf-scm-crunched/CMIP6/" "${WRANGLE_DIR}" "${CONTACT}" --drs "${DRS}" --out-format magicc-input-files-point-end-of-year --regexp "${REGEXP_WRANGLE_IN_FILES}" -f
