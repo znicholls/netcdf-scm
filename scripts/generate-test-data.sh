187e9dcfcc30976aa558824bca9287e41fe4ecd1
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2019-08-17 13:44:22 +1000

    Sort out realm_key

--- scripts/generate-test-data.sh
+++ scripts/generate-test-data.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
-ROOT_DIR="/data/marble/cmip6/CMIP6/CMIP/NCAR/CESM2/historical/r7i1p1f1/"
+ROOT_DIR="/data/marble/cmip5/historical/"
 
 declare -a arr=(
-  "Lmon/gpp/gn/v20190311/gpp_Lmon_CESM2_historical_r7i1p1f1_gn_195001-199912.nc"
-  "Lmon/cSoilFast/gn/v20190311/cSoilFast_Lmon_CESM2_historical_r7i1p1f1_gn_195001-199912.nc"
-  "Amon/tas/gn/v20190311/tas_Amon_CESM2_historical_r7i1p1f1_gn_195001-199912.nc"
-  "Omon/hfds/gn/v20190311/hfds_Omon_CESM2_historical_r7i1p1f1_gn_195001-199912.nc"
-  "Omon/thetao/gn/v20190311/thetao_Omon_CESM2_historical_r7i1p1f1_gn_195001-199912.nc"
-  "fx/areacella/gn/v20190311/areacella_fx_CESM2_historical_r7i1p1f1_gn.nc"
-  "Ofx/areacello/gn/v20190311/areacello_Ofx_CESM2_historical_r7i1p1f1_gn.nc"
-  "Ofx/volcello/gn/v20190311/volcello_Ofx_CESM2_historical_r7i1p1f1_gn.nc"
-  "fx/sftlf/gn/v20190311/sftlf_fx_CESM2_historical_r7i1p1f1_gn.nc"
-  "Ofx/sftof/gn/v20190311/sftof_Ofx_CESM2_historical_r7i1p1f1_gn.nc"
+  "Amon/tas/ACCESS1-0/r1i1p1/tas_Amon_ACCESS1-0_historical_r1i1p1_185001-200512.nc "
+  "Omon/hfds/ACCESS1-0/r1i1p1/hfds_Omon_ACCESS1-0_historical_r1i1p1_185001-200512.nc"
+  "fx/areacella/ACCESS1-0/r0i0p0/areacella_fx_ACCESS1-0_historical_r0i0p0.nc"
+  "fx/areacello/ACCESS1-0/r0i0p0/areacello_fx_ACCESS1-0_historical_r0i0p0.nc"
+  "fx/sftlf/ACCESS1-0/r0i0p0/sftlf_fx_ACCESS1-0_historical_r0i0p0.nc"
+  "fx/sftof/ACCESS1-0/r0i0p0/sftof_fx_ACCESS1-0_historical_r0i0p0.nc"
 )
 
 ## now loop through the above array
@@ -26,8 +22,8 @@ do
      cp $file ${outfile}
    else
       tmpfile=tmp.nc
-      outfile=${basename/195001-199912/195701-195703}
-      cdo -selyear,1957 $file $tmpfile > /dev/null 2>&1
+      outfile=${basename/185001-200512/187701-187703}
+      cdo -selyear,1877 $file $tmpfile > /dev/null 2>&1
       cdo -selmonth,1/3 $tmpfile $outfile > /dev/null 2>&1
       rm $tmpfile > /dev/null 2>&1
    fi
