79d3ede318283fb8590e931fd633e92848b577d3
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2019-08-19 17:26:45 +1000

    Add thetao test files

--- scripts/generate-test-data.sh
+++ scripts/generate-test-data.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 TEST_DIR="./tests/test-data/cmip6output"
-ROOT_DIR="/data/marble/cmip6/CMIP6/CMIP/NCAR/CESM2/historical/r7i1p1f1"
-ROOT_DIR_TO_MAKE="CMIP6/CMIP/NCAR/CESM2/historical/r7i1p1f1"
+ROOT_DIR="/data/marble/cmip6/CMIP6/CMIP/NCAR/CESM2/historical/r10i1p1f1"
+ROOT_DIR_TO_MAKE="CMIP6/CMIP/NCAR/CESM2/historical/r10i1p1f1"
 
 declare -a arr=(
-  "Omon/hfds/gr/v20190311/hfds_Omon_CESM2_historical_r7i1p1f1_gr_195001-199912.nc"
-  "Omon/hfds/gn/v20190311/hfds_Omon_CESM2_historical_r7i1p1f1_gn_195001-199912.nc"
-  "Ofx/areacello/gr/v20190311/areacello_Ofx_CESM2_historical_r7i1p1f1_gr.nc"
-  "Ofx/areacello/gn/v20190311/areacello_Ofx_CESM2_historical_r7i1p1f1_gn.nc"
-  "Ofx/sftof/gn/v20190311/sftof_Ofx_CESM2_historical_r7i1p1f1_gn.nc"
+  "Omon/thetao/gn/v20190313/thetao_Omon_CESM2_historical_r10i1p1f1_gn_195001-199912.nc"
+  "Ofx/areacello/gn/v20190313/areacello_Ofx_CESM2_historical_r10i1p1f1_gn.nc"
+  "Ofx/volcello/gn/v20190313/volcello_Ofx_CESM2_historical_r10i1p1f1_gn.nc"
+  "Ofx/sftof/gn/v20190313/sftof_Ofx_CESM2_historical_r10i1p1f1_gn.nc"
 )
 
 ## now loop through the above array
@@ -23,9 +22,9 @@ do
      cp $file ${outfile}
    else
       tmpfile=tmp.nc
-      outfile=${basename/195001-199912/195701-195703}
-      cdo -selyear,1957 $file $tmpfile > /dev/null 2>&1
-      cdo -selmonth,1/3 $tmpfile $outfile > /dev/null 2>&1
+      outfile=${basename/195001-199912/195310-195312}
+      cdo -selyear,1953 $file $tmpfile > /dev/null 2>&1
+      cdo -selmonth,10/12 $tmpfile $outfile > /dev/null 2>&1
       rm $tmpfile > /dev/null 2>&1
    fi
    echo "mkdir -p ${TEST_DIR}/${ROOT_DIR_TO_MAKE}/${dir_to_make}"
---
+++ tests/test-data/cmip6output/CMIP6/CMIP/NCAR/CESM2/historical/r10i1p1f1/Ofx/volcello/gn/v20190313/volcello_Ofx_CESM2_historical_r10i1p1f1_gn.nc
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b3b083a08a6de7bbef7368bf66bd50493df8e4585c33517cd67f23b88482fbe9
+size 8406595
