09ff4841829fb6e03d3e5200c5031152f78ed7cd
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2019-08-20 15:26:44 +1000

    Add test files

--- scripts/full-process.sh
+++ scripts/full-process.sh
@@ -4,7 +4,8 @@ CRUNCH_DIR="/data/marble/sandbox/share/cmip6-crunched-full-test"
 WRANGLE_DIR="/data/marble/sandbox/share/cmip6-wrangled-full-test"
 CONTACT='Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>, Jared Lewis <jared.lewis@climate-energy-college.org>, Malte Meinshausen <malte.meinshausen@unimelb.edu.au>'
 DRS="CMIP6Output"
-REGEXP="^(?!.*(fx|/ta/|/co2/|tos|hfds)).*(tas/|fco2nat|rsut|rlut|rsdt|ts|fgco2|hfds|hfcorr|tos|fco2fos|fco2nat|fco2antt|co2fs).*gr.*$"
+REGEXP="^(?!.*(fx|/ta/|/co2/|tos)).*(tas/|fco2nat|rsut|rlut|rsdt|ts|fgco2|hfds|hfcorr|tos|fco2fos|fco2nat|fco2antt|co2fs|hfds|thetao).*gr.*$"
+REGEXP="^(?!.*(fx|/ta/|/co2/)).*(tas/|tos|hfds|thetao).*gr.*$"
 # have to be super careful when crunching input files as the duplicate grids can cause
 # things to explode
 REGEXP_WRANGLE_IN_FILES=".*tas/.*gr/.*"
