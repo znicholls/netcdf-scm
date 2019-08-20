443abf3ec39c5cc281efb6486835bde0e4aa65f0
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2019-08-20 18:52:46 +1000

    Update regression tests

--- tests/integration/test_wrangling.py
+++ tests/integration/test_wrangling.py
@@ -202,7 +202,7 @@ def test_wrangling_blend_models(tmpdir, caplog, test_cmip6_crunch_output):
     assert ".*" in result.output
     assert ".mat" in result.output
 
-    assert len(glob(join(OUTPUT_DIR, "*.mat"))) == 107
+    assert len(glob(join(OUTPUT_DIR, "*.mat"))) == 115
 
 
 def test_wrangling_handles_integer_units(tmpdir, caplog, test_cmip6_crunch_output):
