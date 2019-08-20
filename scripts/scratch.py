70be7a9bd12c02497a929aff4d5c3dfc6a69d3d4
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2019-08-20 15:50:35 +1000

    Make black
    Former-commit-id: 1d5080465058d846659255787a5b579ac7b2e66a

--- scripts/scratch.py
+++ scripts/scratch.py
@@ -1,8 +1,14 @@
+import pdb
+
 from netcdf_scm.iris_cube_wrappers import CMIP6OutputCube
 
-test  = CMIP6OutputCube()
+test = CMIP6OutputCube()
 
-import pdb
+
 pdb.set_trace()
-test.load_data_in_directory("/data/marble/cmip6/CMIP6/CMIP/NCAR/CESM2/historical/r10i1p1f1/Omon/tos/gn/v20190313/")
-test.load_data_in_directory("tests/test-data/cmip6output/CMIP6/CMIP/NCAR/CESM2/historical/r10i1p1f1/Omon/tos/gn/v20190313")
+test.load_data_in_directory(
+    "/data/marble/cmip6/CMIP6/CMIP/NCAR/CESM2/historical/r10i1p1f1/Omon/tos/gn/v20190313/"
+)
+test.load_data_in_directory(
+    "tests/test-data/cmip6output/CMIP6/CMIP/NCAR/CESM2/historical/r10i1p1f1/Omon/tos/gn/v20190313"
+)
