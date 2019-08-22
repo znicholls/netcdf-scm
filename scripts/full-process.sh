69fc14b7bf483d768029feb315a7455527bca7a8
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2019-08-23 09:11:25 +1000

    Update processing again

--- scripts/full-process.sh
+++ scripts/full-process.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-SRC_DIR="/media/research-nfs/cmip6/CMIP6"
+SRC_DIR="/data/marble/cmip6/CMIP6"
 CRUNCH_DIR="/data/marble/sandbox/share/cmip6-crunched-full-test"
 WRANGLE_DIR="/data/marble/sandbox/share/cmip6-wrangled-full-test"
 CONTACT='Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>, Jared Lewis <jared.lewis@climate-energy-college.org>, Malte Meinshausen <malte.meinshausen@unimelb.edu.au>'
@@ -11,6 +11,6 @@ REGEXP="^(?!.*(fx|/ta/|/co2/|tos)).*(/ta/|/co2/|/fco2fos/|/fco2nat/|/fco2antt/|/
 # things to explode
 REGEXP_WRANGLE_IN_FILES=".*tas/.*gr/.*"
 
-netcdf-scm-crunch "${SRC_DIR}" "${CRUNCH_DIR}" "${CONTACT}"  --drs "${DRS}" --regexp "${REGEXP}" --small-number-workers 10 --medium-number-workers 2
+netcdf-scm-crunch "${SRC_DIR}" "${CRUNCH_DIR}" "${CONTACT}"  --drs "${DRS}" --regexp "${REGEXP}" --small-number-workers 10 --medium-number-workers 3
 netcdf-scm-wrangle "${CRUNCH_DIR}/netcdf-scm-crunched/CMIP6/" "${WRANGLE_DIR}" "${CONTACT}" --drs "${DRS}" --number-workers 1
 netcdf-scm-wrangle "${CRUNCH_DIR}/netcdf-scm-crunched/CMIP6/" "${WRANGLE_DIR}" "${CONTACT}" --drs "${DRS}" --out-format magicc-input-files-point-end-of-year --regexp "${REGEXP_WRANGLE_IN_FILES}" --number-workers 1
