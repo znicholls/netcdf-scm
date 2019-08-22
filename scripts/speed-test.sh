315cd41a1835a642048a4abef1ab79085181516a
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2019-08-22 21:43:46 +1000

    Update speed test and try fixing cli error returning
    Former-commit-id: 622294ec0d38b7aa009c6d8c0e44742373e9ff8a

--- scripts/speed-test.sh
+++ scripts/speed-test.sh
@@ -13,20 +13,20 @@ exec 3>&- 4>&-
 echo "Multiple small file (2.4 million data points) time: $SRC_DIR_TIME"
 
 
-# SRC_DIR_MEDIUM="/data/marble/cmip6/CMIP6/CMIP/BCC/BCC-CSM2-MR/historical"
+SRC_DIR_MEDIUM="/data/marble/cmip6/CMIP6/CMIP/BCC/BCC-CSM2-MR/historical"
 
-# exec 3>&1 4>&2
-# SRC_DIR_MEDIUM_TIME=$( { time netcdf-scm-crunch "${SRC_DIR_MEDIUM}" "${CRUNCH_DIR}" "${CONTACT}"  --drs "${DRS}" --regexp "${REGEXP}" --force --small-number-workers 10 1>&3 2>&4; } 2>&1 )
-# exec 3>&- 4>&-
-# echo "Multiple medium file (100 million data points) crunch time: $SRC_DIR_MEDIUM_TIME"
+exec 3>&1 4>&2
+SRC_DIR_MEDIUM_TIME=$( { time netcdf-scm-crunch "${SRC_DIR_MEDIUM}" "${CRUNCH_DIR}" "${CONTACT}"  --drs "${DRS}" --regexp "${REGEXP}" --force --small-number-workers 10 1>&3 2>&4; } 2>&1 )
+exec 3>&- 4>&-
+echo "Multiple medium file (100 million data points) crunch time: $SRC_DIR_MEDIUM_TIME"
 
 
-# SRC_DIR_BIG="/data/marble/cmip6/CMIP6/CMIP/BCC/BCC-CSM2-MR/piControl/r1i1p1f1/Amon"
+SRC_DIR_BIG="/data/marble/cmip6/CMIP6/CMIP/BCC/BCC-CSM2-MR/piControl/r1i1p1f1/Amon"
 
-# exec 3>&1 4>&2
-# SRC_DIR_BIG_TIME=$( { time netcdf-scm-crunch "${SRC_DIR_BIG}" "${CRUNCH_DIR}" "${CONTACT}"  --drs "${DRS}" --regexp "${REGEXP}" --force --medium-number-workers 2 1>&3 2>&4; } 2>&1 )
-# exec 3>&- 4>&-
-# echo "Multiple large files (300 million data points) crunch time: $SRC_DIR_BIG_TIME"
+exec 3>&1 4>&2
+SRC_DIR_BIG_TIME=$( { time netcdf-scm-crunch "${SRC_DIR_BIG}" "${CRUNCH_DIR}" "${CONTACT}"  --drs "${DRS}" --regexp "${REGEXP}" --force --medium-number-workers 2 1>&3 2>&4; } 2>&1 )
+exec 3>&- 4>&-
+echo "Multiple large files (300 million data points) crunch time: $SRC_DIR_BIG_TIME"
 
 
 SRC_DIR_OCEAN_2D="/data/marble/sandbox/znicholls/test-cmip6output/CMIP6"
@@ -37,12 +37,12 @@ exec 3>&- 4>&-
 echo "Multiple ocean 2D files (240 million data points) crunch time: $SRC_DIR_OCEAN_2D"
 
 
-#SRC_DIR_ENORMOUS="/data/marble/sandbox/znicholls/test-cmip6output/CMIP6"
+SRC_DIR_ENORMOUS="/data/marble/sandbox/znicholls/test-cmip6output/CMIP6"
 
-#exec 3>&1 4>&2
-#SRC_DIR_ENORMOUS_TIME=$( { time netcdf-scm-crunch "${SRC_DIR_ENORMOUS}" "${CRUNCH_DIR}" "${CONTACT}"  --drs "${DRS}" --regexp ".*thetao.*" --force --medium-number-workers 2 1>&3 2>&4; } 2>&1 )
-#exec 3>&- 4>&-
-#echo "Multiple enormous files (14 000 million data points) crunch time: $SRC_DIR_ENORMOUS_TIME"
+exec 3>&1 4>&2
+SRC_DIR_ENORMOUS_TIME=$( { time netcdf-scm-crunch "${SRC_DIR_ENORMOUS}" "${CRUNCH_DIR}" "${CONTACT}"  --drs "${DRS}" --regexp '.*thetao.*' --force --medium-number-workers 2 1>&3 2>&4; } 2>&1 )
+exec 3>&- 4>&-
+echo "Multiple enormous files (14 000 million data points) crunch time: $SRC_DIR_ENORMOUS_TIME"
 
 
 exec 3>&1 4>&2
