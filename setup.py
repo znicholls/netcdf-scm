1f4162b03ce494dc3c61c7f70a48be8fc2007d35
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2019-09-03 15:23:32 +1000

    Prepare for release of v2.0.0alpha

--- setup.py
+++ setup.py
@@ -42,7 +42,6 @@ ENTRY_POINTS = {
 
 REQUIREMENTS_INSTALL = [
     "numpy",
-    "pandas",
     "pyam-iamc>=0.2.0",
     "pandas==0.24.2",  # pyam issue, see https://github.com/IAMconsortium/pyam/issues/245
     "python-dateutil",
@@ -50,6 +49,7 @@ REQUIREMENTS_INSTALL = [
     "openscm>=0.1.0a",
     "pymagicc>=2.0.0b4",
     "click",
+    "tqdm",
 ]
 REQUIREMENTS_TESTS = ["codecov", "pytest-cov", "pytest>=4.0,<5.0"]
 REQUIREMENTS_NOTEBOOKS = ["notebook", "nbval", "expectexception"]
