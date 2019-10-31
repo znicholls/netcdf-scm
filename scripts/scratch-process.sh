778b9c01ccc83d3f1d6e9cf2694fb4e291596833
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2019-11-01 00:55:06 +1100

    Fix scratch script

--- scripts/scratch-process.sh
+++ scripts/scratch-process.sh
@@ -12,4 +12,4 @@ REGEXP_WRANGLE_IN_FILES="^((?!historical).)*tas/.*gr/.*"
 netcdf-scm-crunch "${SRC_DIR}" "${CRUNCH_DIR}" "${CONTACT}"  --drs "${DRS}" --regexp "${REGEXP}" -f
 netcdf-scm-crunch "${SRC_DIR}" "${CRUNCH_DIR}" "${CONTACT}"  --drs "${DRS}" --regexp "${REGEXP}" -f --small-number-workers 1
 netcdf-scm-wrangle "${CRUNCH_DIR}/netcdf-scm-crunched/CMIP6/" "${WRANGLE_DIR}" "${CONTACT}" --drs "${DRS}" -f
-netcdf-scm-wrangle "${CRUNCH_DIR}/netcdf-scm-crunched/CMIP6/" "${WRANGLE_DIR}" "${CONTACT}" --drs "${DRS}" --out-format magicc-input-files-point-end-of-year --regexp "${REGEXP_WRANGLE_IN_FILES}" -f
+netcdf-scm-wrangle "${CRUNCH_DIR}/netcdf-scm-crunched/CMIP6/" "${WRANGLE_DIR}" "${CONTACT}" --drs "${DRS}" --out-format magicc-input-files-point-end-year --regexp "${REGEXP_WRANGLE_IN_FILES}" -f
