0347c6c6c91adfb79dc42c24661f656974180f6d
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2019-11-01 16:01:11 +1100

    Get mag file wrangling working in parallel

--- scripts/scratch.py
+++ scripts/scratch.py
@@ -5,12 +5,13 @@ from netcdf_scm.cli import _do_wrangling
 _do_wrangling(
     os.path.join("./tests/test-data/expected-crunching-output/cmip6output/CMIP6/"),
     os.path.join("output-examples/wrangling-scratch"),
-    "^(?!.*(fx)).*$",
+    # "^(?!.*(fx)).*$",
+    "^(?!.*(fx)).*tas.*$",
     "mag-files",
     # "mag-files-average-year-mid-year",
     True,
     "Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>, Jared Lewis <jared.lewis@climate-energy-college.org>, Malte Meinshausen <malte.meinshausen@unimelb.edu.au>",
     "CMIP6Output",
-    1,
+    5,
     None,
 )
