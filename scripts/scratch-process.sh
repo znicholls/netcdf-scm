ecf1682dc6a6ea344332808f7850da82a62595df
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2019-11-08 18:44:13 +1100

    Remove time from MAGICC IN files

--- scripts/scratch-process.sh
+++ scripts/scratch-process.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
-SRC_DIR="tests/test-data/cmip6output"
+SRC_DIR="tests/test-data/cmip6output/"
 CRUNCH_DIR="output-examples/scratch-process-output"
 WRANGLE_DIR="output-examples/wrangle-process-output"
+STITCH_DIR="output-examples/stitch-process-output"
 CONTACT='Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>, Jared Lewis <jared.lewis@climate-energy-college.org>, Malte Meinshausen <malte.meinshausen@unimelb.edu.au>'
 DRS="CMIP6Output"
-REGEXP="^(?!.*fx).*IPSL.*$"
-# have to be super careful when crunching input files as the duplicate grids can cause
-# things to explode
-REGEXP_WRANGLE_IN_FILES="^((?!historical).)*tas/.*gr/.*"
+
+REGEXP="^(?!.*(fx|thetao)).*$"
+# can't handle thetao wrangling/stitching yet...
+REGEXP_WRANGLE_IN_FILES=".*BCC-CSM2-MR.*tas"
+REGEXP_STITCH_IN_FILES=".*BCC-CSM2-MR.*tas"
 
 netcdf-scm-crunch "${SRC_DIR}" "${CRUNCH_DIR}" "${CONTACT}"  --drs "${DRS}" --regexp "${REGEXP}" -f
 netcdf-scm-crunch "${SRC_DIR}" "${CRUNCH_DIR}" "${CONTACT}"  --drs "${DRS}" --regexp "${REGEXP}" -f --small-number-workers 1
+
 netcdf-scm-wrangle "${CRUNCH_DIR}/netcdf-scm-crunched/CMIP6/" "${WRANGLE_DIR}" "${CONTACT}" --drs "${DRS}" -f
 netcdf-scm-wrangle "${CRUNCH_DIR}/netcdf-scm-crunched/CMIP6/" "${WRANGLE_DIR}" "${CONTACT}" --drs "${DRS}" --out-format magicc-input-files-point-end-year --regexp "${REGEXP_WRANGLE_IN_FILES}" -f
+
+netcdf-scm-stitch "${CRUNCH_DIR}/netcdf-scm-crunched/CMIP6/" "${STITCH_DIR}" "${CONTACT}" --drs "${DRS}" --regexp "${REGEXP_STITCH_IN_FILES}" -f
+netcdf-scm-stitch "${CRUNCH_DIR}/netcdf-scm-crunched/CMIP6/" "${STITCH_DIR}" "${CONTACT}" --drs "${DRS}" --out-format magicc-input-files-point-end-year --regexp "${REGEXP_STITCH_IN_FILES}" -f
