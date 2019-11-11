6e222acb732b95ac58387803f9d48b1358f5c3c6
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2019-11-11 21:02:18 +1100

    Setup speed test

--- scripts/speed-test-crunch.sh
+++ scripts/speed-test-crunch.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-CRUNCH_DIR="/data/marble/sandbox/share/cmip6-crunched-ipsl-sandbox-speed-test"
-CONTACT='Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>, Jared Lewis <jared.lewis@climate-energy-college.org>, Malte Meinshausen <malte.meinshausen@unimelb.edu.au>'
+CRUNCH_DIR="/home/ubuntu/data/cmip6-speed-test-output"
+CONTACT='CMIP6 data crunching speed test'
 DRS="CMIP6Output"
 REGEXP='^(?!.*(fx|/co2/|/ta/)).*Amon.*$'
 
+SRC_DIR_ROOT="/home/ubuntu/data/cmip6-speed-test"
 
-SRC_DIR="/data/marble/cmip6/CMIP6/DCPP/IPSL/IPSL-CM6A-LR/dcppC-atl-control"
+SRC_DIR="${SRC_DIR_ROOT}/CMIP6/DCPP/IPSL/IPSL-CM6A-LR/dcppC-atl-control"
 
 exec 3>&1 4>&2
 SRC_DIR_TIME=$( { time netcdf-scm-crunch "${SRC_DIR}" "${CRUNCH_DIR}" "${CONTACT}"  --drs "${DRS}" --regexp "${REGEXP}" --force --small-number-workers 15 1>&3 2>&4; } 2>&1 )
@@ -13,7 +14,7 @@ exec 3>&- 4>&-
 echo "Multiple small file (2.4 million data points) time: $SRC_DIR_TIME"
 
 
-SRC_DIR_MEDIUM="/data/marble/cmip6/CMIP6/CMIP/BCC/BCC-CSM2-MR/historical"
+SRC_DIR_MEDIUM="${SRC_DIR_ROOT}/CMIP6/CMIP/BCC/BCC-CSM2-MR/historical"
 
 exec 3>&1 4>&2
 SRC_DIR_MEDIUM_TIME=$( { time netcdf-scm-crunch "${SRC_DIR_MEDIUM}" "${CRUNCH_DIR}" "${CONTACT}"  --drs "${DRS}" --regexp "${REGEXP}" --force --small-number-workers 10 1>&3 2>&4; } 2>&1 )
@@ -21,28 +22,29 @@ exec 3>&- 4>&-
 echo "Multiple medium file (100 million data points) crunch time: $SRC_DIR_MEDIUM_TIME"
 
 
-SRC_DIR_BIG="/data/marble/cmip6/CMIP6/CMIP/BCC/BCC-CSM2-MR/piControl/r1i1p1f1/Amon"
+SRC_DIR_BIG="${SRC_DIR_ROOT}/CMIP6/CMIP/BCC/BCC-CSM2-MR/piControl/r1i1p1f1/Amon"
 
 exec 3>&1 4>&2
 SRC_DIR_BIG_TIME=$( { time netcdf-scm-crunch "${SRC_DIR_BIG}" "${CRUNCH_DIR}" "${CONTACT}"  --drs "${DRS}" --regexp "${REGEXP}" --force --medium-number-workers 2 1>&3 2>&4; } 2>&1 )
 exec 3>&- 4>&-
 echo "Multiple large files (300 million data points) crunch time: $SRC_DIR_BIG_TIME"
 
+# Ocean data, future problem
 
-SRC_DIR_OCEAN_2D="/data/marble/sandbox/znicholls/test-cmip6output/CMIP6"
+# SRC_DIR_OCEAN_2D="${SRC_DIR_ROOT}//CMIP6"
 
-exec 3>&1 4>&2
-SRC_DIR_OCEAN_2D_TIME=$( { time netcdf-scm-crunch "${SRC_DIR_OCEAN_2D}" "${CRUNCH_DIR}" "${CONTACT}"  --drs "${DRS}" --regexp '.*zos.*' --force --medium-number-workers 2 1>&3 2>&4; } 2>&1 )
-exec 3>&- 4>&-
-echo "Multiple ocean 2D files (240 million data points) crunch time: $SRC_DIR_OCEAN_2D_TIME"
+# exec 3>&1 4>&2
+# SRC_DIR_OCEAN_2D_TIME=$( { time netcdf-scm-crunch "${SRC_DIR_OCEAN_2D}" "${CRUNCH_DIR}" "${CONTACT}"  --drs "${DRS}" --regexp '.*zos.*' --force --medium-number-workers 2 1>&3 2>&4; } 2>&1 )
+# exec 3>&- 4>&-
+# echo "Multiple ocean 2D files (240 million data points) crunch time: $SRC_DIR_OCEAN_2D_TIME"
 
 
-SRC_DIR_ENORMOUS="/data/marble/sandbox/znicholls/test-cmip6output/CMIP6"
+# SRC_DIR_ENORMOUS="${SRC_DIR_ROOT}//CMIP6"
 
-exec 3>&1 4>&2
-SRC_DIR_ENORMOUS_TIME=$( { time netcdf-scm-crunch "${SRC_DIR_ENORMOUS}" "${CRUNCH_DIR}" "${CONTACT}"  --drs "${DRS}" --regexp '.*thetao.*' --force --medium-number-workers 2 1>&3 2>&4; } 2>&1 )
-exec 3>&- 4>&-
-echo "Multiple enormous files (14 000 million data points) crunch time: $SRC_DIR_ENORMOUS_TIME"
+# exec 3>&1 4>&2
+# SRC_DIR_ENORMOUS_TIME=$( { time netcdf-scm-crunch "${SRC_DIR_ENORMOUS}" "${CRUNCH_DIR}" "${CONTACT}"  --drs "${DRS}" --regexp '.*thetao.*' --force --medium-number-workers 2 1>&3 2>&4; } 2>&1 )
+# exec 3>&- 4>&-
+# echo "Multiple enormous files (14 000 million data points) crunch time: $SRC_DIR_ENORMOUS_TIME"
 
 echo ""
 echo ""
@@ -55,5 +57,5 @@ echo "$VERSION"
 echo "Multiple small file (2.4 million data points) time: $SRC_DIR_TIME"
 echo "Multiple medium file (100 million data points) crunch time: $SRC_DIR_MEDIUM_TIME"
 echo "Multiple big file (300 million data points) crunch time: $SRC_DIR_BIG_TIME"
-echo "Multiple ocean 2D file (240 million data points) crunch time: $SRC_DIR_OCEAN_2D_TIME"
-echo "Multiple enormous file (14 000 million data points) crunch time: $SRC_DIR_ENORMOUS_TIME"
+# echo "Multiple ocean 2D file (240 million data points) crunch time: $SRC_DIR_OCEAN_2D_TIME"
+# echo "Multiple enormous file (14 000 million data points) crunch time: $SRC_DIR_ENORMOUS_TIME"
